f7e424d809c1631885d5dafb8a0e91dc3012fa01
Renan Nascimento <renan@costanascimento.com.br>
2015-08-04 12:39:12 -0300

    Crud de professor e atualização do modelo.

--- client/js/controllers/controllers.js
+++ client/js/controllers/controllers.js
@@ -26,6 +26,47 @@ app.controller('ControladorDisciplinas', ['$scope', 'Disciplina', function($scop
 
 app.controller('ControladorProfessor', ['$scope', 'Professor', function($scope, Professor){
 
-  
+  $scope.professores = Professor.find();
+  $scope.professor = {};
+
+  $scope.preencherEndereco = function(cep){
+    if (cep.length != 8)
+    {
+      Materialize.toast('Formato do CEP inválido.', 3000);
+      return;
+    }
+
+    var endereco_servico = "http://cep.correiocontrol.com.br/" + cep + ".json";
+
+    $.ajax({url: endereco_servico}).success(function(objEndereco){
+      $scope.professor.bairro = objEndereco.bairro;
+      $scope.professor.rua = objEndereco.logradouro;
+      $scope.professor.estado = objEndereco.uf;
+      $scope.professor.cidade = objEndereco.localidade;
+      $scope.professor.numero = 10;
+      $scope.$apply();
+    }).fail(function() {
+      Materialize.toast('CEP não encontrado.', 3000);
+    });
+  }
+
+  $scope.salvarProfessor = function(){
+    $scope.professores.push(Professor.upsert($scope.professor));
+    Materialize.toast('Professor cadastrado com sucesso.', 3000);
+  }
+
+  $scope.editarProfessor = function(){
+    Professor.upsert($scope.professor);
+    Materialize.toast('Professor editado com sucesso.', 3000);
+  }
+
+  $scope.carregarEdicao = function(professor){
+    $scope.professor = professor;
+    $scope.professor.nascimento = new Date(professor.nascimento);
+  }
+
+  $scope.limpar = function(){
+    $scope.professor = {};
+  }
 
 }]);
